14a8be694eae0b8a6ed0b9bd3daed52048ecb1a8
maikhain74 <maikhain@gmail.com>
2026-05-04 15:29:17 +0200

    Stripe PayPal vorbereitet und Buildfehler behoben

--- src/pages/api/checkout.ts
+++ src/pages/api/checkout.ts
@@ -5,12 +5,18 @@ const stripe = new Stripe(import.meta.env.STRIPE_SECRET_KEY);
 export async function GET() {
   const session = await stripe.checkout.sessions.create({
     mode: "payment",
+
+    automatic_payment_methods: {
+      enabled: true,
+    },
+
     line_items: [
       {
         price: import.meta.env.STRIPE_PRICE_ID,
         quantity: 1,
       },
     ],
+
     success_url: `${import.meta.env.SITE_URL}/erfolg?session_id={CHECKOUT_SESSION_ID}`,
     cancel_url: `${import.meta.env.SITE_URL}/premium`,
   });
