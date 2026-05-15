80518b82180837d5850293d9b6d0cf2b9de8f001
Mac <maxmustermann@iMac.fritz.box>
2026-05-15 19:49:51 +0200

    Add sitemap integration

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,9 @@
 import { defineConfig } from 'astro/config';
 import vercel from '@astrojs/vercel';
+import sitemap from '@astrojs/sitemap';
 
 export default defineConfig({
-  output: 'server',
+  site: 'https://www.praxismaik.de',
+  integrations: [sitemap()],
   adapter: vercel()
 });
