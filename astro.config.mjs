8c3633c584365dfd827e3d3a857161316c9dc5b4
Mac <maxmustermann@Mac.fritz.box>
2026-09-06 16:07:00 +0200

    Fix Pflegeprozess legacy redirects

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,34 +6,6 @@ import sitemap from '@astrojs/sitemap';
 
 export default defineConfig({
   site: 'https://www.praxismaik.de',
-
-  redirects: {
-    '/themen/pflegeprozess/durchfuehrung/':
-      '/themen/pflegeausbildung/pflegeprozess/durchfuehrung/',
-
-    '/themen/pflegeprozess/evaluation/':
-      '/themen/pflegeausbildung/pflegeprozess/evaluation/',
-
-    '/themen/pflegeprozess/informationssammlung/':
-      '/themen/pflegeausbildung/pflegeprozess/informationssammlung/',
-
-    '/themen/pflegeprozess/pflegediagnosen/':
-      '/themen/pflegeausbildung/pflegeprozess/pflegediagnosen/',
-
-    '/themen/pflegeprozess/pflegemassnahmen/':
-      '/themen/pflegeausbildung/pflegeprozess/pflegemassnahmen/',
-
-    '/themen/pflegeprozess/pflegeprobleme-ressourcen/':
-      '/themen/pflegeausbildung/pflegeprozess/pflegeprobleme-ressourcen/',
-
-    '/themen/pflegeprozess/pflegeprozess-grundlagen/':
-      '/themen/pflegeausbildung/pflegeprozess/pflegeprozess-grundlagen/',
-
-    '/themen/pflegeprozess/pflegeziele/':
-      '/themen/pflegeausbildung/pflegeprozess/pflegeziele/',
-  },
-
   integrations: [sitemap()],
-
   adapter: vercel()
 });
