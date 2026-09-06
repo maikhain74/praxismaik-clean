3e65198a3221925e5b7f03ecb0efe19c8331a3d9
Mac <maxmustermann@Mac.fritz.box>
2026-09-06 15:25:02 +0200

    Add permanent Pflegeprozess legacy redirects

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,39 @@
 import { defineConfig } from 'astro/config';
+
 import vercel from '@astrojs/vercel';
+
 import sitemap from '@astrojs/sitemap';
 
 export default defineConfig({
   site: 'https://www.praxismaik.de',
+
+  redirects: {
+    '/themen/pflegeprozess/durchfuehrung/':
+      '/themen/pflegeausbildung/pflegeprozess/durchfuehrung/',
+
+    '/themen/pflegeprozess/evaluation/':
+      '/themen/pflegeausbildung/pflegeprozess/evaluation/',
+
+    '/themen/pflegeprozess/informationssammlung/':
+      '/themen/pflegeausbildung/pflegeprozess/informationssammlung/',
+
+    '/themen/pflegeprozess/pflegediagnosen/':
+      '/themen/pflegeausbildung/pflegeprozess/pflegediagnosen/',
+
+    '/themen/pflegeprozess/pflegemassnahmen/':
+      '/themen/pflegeausbildung/pflegeprozess/pflegemassnahmen/',
+
+    '/themen/pflegeprozess/pflegeprobleme-ressourcen/':
+      '/themen/pflegeausbildung/pflegeprozess/pflegeprobleme-ressourcen/',
+
+    '/themen/pflegeprozess/pflegeprozess-grundlagen/':
+      '/themen/pflegeausbildung/pflegeprozess/pflegeprozess-grundlagen/',
+
+    '/themen/pflegeprozess/pflegeziele/':
+      '/themen/pflegeausbildung/pflegeprozess/pflegeziele/',
+  },
+
   integrations: [sitemap()],
+
   adapter: vercel()
 });
