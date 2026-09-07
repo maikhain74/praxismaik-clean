ee13e86a7adf8ea41bdb365ddc3a3411c6e97764
Mac <maxmustermann@Mac.fritz.box>
2026-09-07 05:18:32 +0200

    Redirect duplicate asthma exam case

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,16 @@
 import { defineConfig } from 'astro/config';
-
 import vercel from '@astrojs/vercel';
-
 import sitemap from '@astrojs/sitemap';
 
 export default defineConfig({
   site: 'https://www.praxismaik.de',
+
+  redirects: {
+    '/pruefung/fallbeispiele/fall-behinderung-kommunikation-eskalation/':
+      '/pruefung/fallbeispiele/fall-asthma-jugendlicher-nonadherence/',
+  },
+
   integrations: [sitemap()],
-  adapter: vercel()
+
+  adapter: vercel(),
 });
